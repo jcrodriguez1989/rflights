8d7447000cf5788d6cc750e7ab512000d7abac1c
Juan Cruz Rodriguez <jcrodriguez@bdmg.com.ar>
2019-05-14 09:12:26 -0300

    Adding skip_on_cran to tests

--- tests/testthat/test-get_flights.R
+++ tests/testthat/test-get_flights.R
@@ -2,11 +2,13 @@ context("get_flights")
 
 test_that("can find flights", {
   # testthat::skip_if_offline()
+  skip_on_cran()
   flights <- get_flights("Cordoba")
   expect_true(length(flights) > 0)
 })
 
 test_that("no flights for wrong location", {
   # testthat::skip_if_offline()
+  skip_on_cran()
   expect_error(get_flights("WrongLocation"))
 })
