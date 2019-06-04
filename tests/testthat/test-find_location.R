014a1e422bbce474a163a53a3dd513175deff606
Juan Cruz Rodriguez <jcrodriguez@bdmg.com.ar>
2019-06-04 12:49:29 -0300

    Fixing tests.

--- tests/testthat/test-find_location.R
+++ tests/testthat/test-find_location.R
@@ -1,14 +1,14 @@
 context("find_location")
 
 test_that("can find Cordoba location", {
-  # testthat::skip_if_offline()
+  skip_if_offline()
   skip_on_cran()
   loc <- find_location("Cordoba")
   expect_true(length(loc) > 0)
 })
 
 test_that("no locations for wrong char", {
-  # testthat::skip_if_offline()
+  skip_if_offline()
   skip_on_cran()
   loc <- find_location("WrongLocation")
   expect_true(length(loc) == 0)
--- tests/testthat/test-get_flights.R
+++ tests/testthat/test-get_flights.R
@@ -1,14 +1,14 @@
 context("get_flights")
 
 test_that("can find flights", {
-  # testthat::skip_if_offline()
+  skip_if_offline()
   skip_on_cran()
-  flights <- get_flights("Cordoba")
+  flights <- get_flights(asia[[1]], oceania[[1]])
   expect_true(length(flights) > 0)
 })
 
 test_that("no flights for wrong location", {
-  # testthat::skip_if_offline()
+  skip_if_offline()
   skip_on_cran()
   expect_error(get_flights("WrongLocation"))
 })
